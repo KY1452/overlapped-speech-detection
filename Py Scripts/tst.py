f03ecace6f76ab1b911b719c198c203e5e54a1b9
Kanishk Yadav <93343589+KY1452@users.noreply.github.com>
2024-08-20 19:52:37 +0530

    Revert "Add files via upload"
    This reverts commit 0d23b05e292ffa82c66463fcafbba967ff145941.

--- Py Scripts/tst.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-file_path = "/home/users/ntu/scsekyad/scratch/OSD/final deployment package/klass-osd-kedro-pipeline/klass-osd/data/02_intermediate/Libri2Mix/wav16k/max/metadata/train_360_clean_part21_2mix_osd_labels.parquet.gzip"
-data = pd.read_parquet(file_path)
-print(data.head())
-print(data.info())
